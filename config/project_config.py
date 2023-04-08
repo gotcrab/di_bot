6013df75ef0c0c461b8a07d71ff6fd48139c4147
gotcrab <kuznetsov_alx@proton.me>
2023-04-08 21:40:51 +0300

    init

---
+++ config/project_config.py
@@ -0,0 +1,23 @@
+from dataclasses import dataclass
+from environs import Env
+
+@dataclass
+class DatabaseConfig:
+    database: str
+    db_host: str
+    db_user: str
+    pass
+
+@dataclass
+class TgBot:
+    token: str
+    adm_ids: list[int]
+
+class Config:
+    tg_bot: TgBot
+    # db: DatabaseConfig
+
+def load_config(path: str | None='/home/gotcrab/PycharmProjects/di_bot/.env') -> Config:
+    env: Env = Env()
+    env.read_env(path)
+    return Config(tg_bot = TgBot(token=env('BOT_TOKEN')))
